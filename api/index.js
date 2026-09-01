b808228f61f0a18afab7d2b8bbe36b38e3a924b4
FT-Key <fr4nc0t2@gmail.com>
2026-08-31 21:26:09 -0300

    fix: connect to MongoDB in serverless handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
+const connectDB = require('../src/config/db');
 const app = require('../src/app');
 
-module.exports = app;
+const handler = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
+
+module.exports = handler;
